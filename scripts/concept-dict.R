d7934040c47a7f7d30fa6964460966bade1945e2
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-08-29 16:15:36 +0200

    update aumc concept names

--- scripts/concept-dict.R
+++ scripts/concept-dict.R
@@ -2,118 +2,115 @@
 r_dir <- file.path(rprojroot::find_root(".git/index"), "r")
 invisible(lapply(list.files(r_dir, full.names = TRUE), source))
 
-wrap_list <- function(x, ...) {
-  x <- lapply(x, as.integer)
-  x <- lapply(x, sort)
-  x <- Map(list, ids = x, ...)
+wrap_list <- function(...) {
+  x <- Map(list, ...)
   x <- lapply(x, list)
   x <- Map(list, aumc = x)
   Map(list, sources = x)
 }
 
+prep_ids <- function(x) lapply(lapply(x, as.integer), sort)
+
+strip_ws <- function(x) gsub("\\s+", " ", x)
+
 num_itms <- list(
-  hematocrit = c(11423, 11545, 6777),
-  platelet_count = c(9964, 6797, 10409, 14252),
-  white_blood_cells = c(9965, 6779),
-  sodium = c(9924, 6840, 9555, 10284),
-  potassium = c(9927, 9556, 6835, 10285),
-  c_reactive_protein = c(10079, 6825),
-  creatine_kinase = c(11998, 6822),
-  alanine_aminotransferase = c(11978, 6800),
-  asparate_aminotransferase = c(11990, 6806),
-  lactate = c(10053, 6837, 9580),
-  alkaline_phosphatase = c(11984, 6803),
-  calcium_ionized = c(10267, 6815, 9560, 8915, 9561),
+  hct = c(11423, 11545, 6777),
+  plt = c(9964, 6797, 10409, 14252),
+  wbc = c(9965, 6779),
+  na = c(9924, 6840, 9555, 10284),
+  k = c(9927, 9556, 6835, 10285),
+  crp = c(10079, 6825),
+  ck = c(11998, 6822),
+  alt = c(11978, 6800),
+  ast = c(11990, 6806),
+  lact = c(10053, 6837, 9580),
+  alp = c(11984, 6803),
+  cai = c(10267, 6815, 9560, 8915, 9561),
   ph = c(12310, 6848),
-  pa_co2 = c(6846, 9990, 21213),
-  pa_o2 = c(7433, 9996, 21214),
-  base_excess = c(9994, 6807),
-  bicarbonate = c(9992, 6810),
-  anion_gap = c(9559, 8492),
-  o2_saturation = c(6709, 12311, 8903),
-  fi_o2 = 12279,
-  chloride = 9930,
+  pco2 = c(6846, 9990, 21213),
+  po2 = c(7433, 9996, 21214),
+  be = c(9994, 6807),
+  bicar = c(9992, 6810),
+# anion_gap = c(9559, 8492),
+  o2sat = c(6709, 12311, 8903),
+  fio2 = 12279,
+  cl = 9930,
   mcv = 9968,
-  lymphocytes = 14258,
+  lymph = 14258,
   inr_pt = 11893,
   ptt = 17982,
-  basophils = 14256,
-  troponin_t = 10407,
-  methemoglobin = 11692,
-  heart_rate = 6640,
-  respiratory_rate = c(8874, 12266),
-  systolic_bp = 6641,
-  mean_bp = 6642,
-  diastolic_bp = 6643,
-  urine_out = 8794,
-  gcs_total = 6732,
-  gcs_eye = 13077,
-  gcs_verbal = 13066,
-  gcs_motor = 13072
+  basos = 14256,
+  tnt = 10407,
+  methb = 11692,
+  hr = 6640,
+  resp = c(8874, 12266),
+  sbp = 6641,
+  map = 6642,
+  dbp = 6643,
+  urine = 8794,
+  tgcs = 6732,
+  egcs = 13077,
+  vgcs = 13066,
+  mgcs = 13072
 )
 
-num_itms <- wrap_list(num_itms, table = "numericitems", sub_var = "itemid")
+num_itms <- wrap_list(ids = prep_ids(num_itms), table = "numericitems",
+                      sub_var = "itemid")
 
 cbk_itms <- list(
-  albumin = c(9937, 6801),
-  bilirubin_total = c(9945, 6813),
-  calcium = c(9933, 6817),
-  creatinine = c(9941, 6836, 14216),
-  glucose = c(9947, 6833, 9557),
-  hemoglobin = c(9960, 6778, 10286, 19703, 9553),
-  magnesium = c(9952, 6839),
+  alb = c(9937, 6801),
+  bili = c(9945, 6813),
+  ca = c(9933, 6817),
+  crea = c(9941, 6836, 14216),
+  glu = c(9947, 6833, 9557),
+  hgb = c(9960, 6778, 10286, 19703, 9553),
+  mg = c(9952, 6839),
   mchc = 18666,
   mch = 11679,
-  phosphate = c(9935, 6828),
-  urea_nitrogen = c(9943, 6850)
+  phos = c(9935, 6828),
+  bun = c(9943, 6850)
 )
 
-cbk_itms <- wrap_list(cbk_itms, table = "numericitems", sub_var = "itemid",
-  callback = c(
-    "multiply_aumc_albu", "multiply_aumc_bili", "multiply_aumc_calc",
-    "multiply_aumc_crea", "multiply_aumc_gluc", "multiply_aumc_hemo",
-    "multiply_aumc_magn", "multiply_aumc_mchc", "multiply_aumc_mch",
-    "multiply_aumc_phos", "multiply_aumc_urea"
+cbk_itms <- wrap_list(ids = prep_ids(cbk_itms), table = "numericitems",
+  sub_var = "itemid", callback = c(
+    "transform_fun(binary_op(`*`, 0.1))",
+    "transform_fun(binary_op(`*`, 0.058467))",
+    "transform_fun(binary_op(`*`, 4.008))",
+    "transform_fun(binary_op(`*`, 0.011309))",
+    "transform_fun(binary_op(`*`, 18.016))",
+    "transform_fun(binary_op(`*`, 1.611344))",
+    "transform_fun(binary_op(`*`, 2.431))",
+    "transform_fun(binary_op(`*`, 1.611344))",
+    "transform_fun(binary_op(`*`, 0.016114))",
+    "transform_fun(binary_op(`*`, 3.097521))",
+    "transform_fun(binary_op(`*`, 2.8))"
   )
 )
 
 drg_itms <- list(
-  norepinephrine = 8676,
-  dopamine = 6983,
-  insulin = c(6929, 4218, 2663),
-  dobutamine = 1442
+  norepi = 8676,
+  dopa = 6983,
+  ins = c(6929, 4218, 2663),
+  dobu = 1442
 )
 
-drg_itms <- wrap_list(drg_itms, table = "drugitems", sub_var = "itemid")
+drg_itms <- wrap_list(ids = prep_ids(drg_itms), table = "drugitems",
+                      sub_var = "itemid")
 
-cfg <- list(
-  age = list(
-    sources = list(
-      aumc = list(
-        list(table = "admissions", itm_vars = "agegroup",
-             callback = "aumc_age", class = "col_itm")
-      )
+dem_itms <- wrap_list(table = "admissions",
+  itm_vars = c("agegroup", "weightgroup", "gender"),
+  callback = strip_ws(
+    c("apply_map(c(`18-39` = 30, `40-49`   = 45, `50-59` = 55, `60-69` = 65,
+                   `70-79` = 75, `80+`     = 90))",
+      "apply_map(c(`59-`   = 50, `60-69`   = 65, `70-79` = 75, `80-89` = 85,
+                  `90-99`  = 95, `100-109` = 105, `110+` = 120))",
+      "apply_map(c(Vrouw   = 'Female', Man = 'Male'))"
     )
   ),
-  weight = list(
-    sources = list(
-      aumc = list(
-        list(table = "admissions", itm_vars = "weightgroup",
-             callback = "aumc_weight", class = "col_itm")
-      )
-    )
-  ),
-  sex = list(
-    sources = list(
-      aumc = list(
-        list(table = "admissions", itm_vars = "gender",
-             callback = "aumc_sex", class = "col_itm")
-      )
-    )
-  )
+  class = "col_itm"
 )
 
-cfg <- c(cbk_itms, num_itms, drg_itms, cfg)
+cfg <- c(cbk_itms, num_itms, drg_itms, dem_itms)
 cfg <- cfg[order(names(cfg))]
 
 cfg <- lapply(cfg, function(x) {
